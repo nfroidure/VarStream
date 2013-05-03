81641e0dfdbe6c4a81f5d223b8d447301b7fea3a
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-05-03 11:52:11 +0200

    Updated comparison scripts

--- compare/make_json.js
+++ compare/make_json.js
@@ -1,21 +1,36 @@
 var VarStream = require('../VarStream');
 var fs = require('fs');
 
-var scope = {};
-var myVarStream=new VarStream(scope, true);
+var scope, myVarStream;
 if(process.argv[2])
 	{
-	fs.createReadStream(__dirname+'/'+process.argv[2]).pipe(myVarStream) 
-	  .on('end', function () {
-		console.log(scope);
-		fs.writeFile(__dirname+"/"+process.argv[2].split('.')[0]+".json", JSON.stringify(scope), function(err) {
-			if(err) {
-				console.log(err);
-			} else {
-				console.log("Saved!");
-			}
-		});
-		});
+	if(process.argv[3])
+		{
+		fs.readFile(__dirname+'/'+process.argv[2], function read(err, data)
+			{
+			if (err) { throw err; }
+			scope=JSON.parse(data);
+			myVarStream=new VarStream(scope, true);
+			myVarStream.pipe(fs.createWriteStream(__dirname+"/"+process.argv[2].split('.')[0]+".dat"));
+			console.log("Saved!");
+			});
+		}
+	else
+		{
+		scope = {};
+		myVarStream=new VarStream(scope, true);
+		fs.createReadStream(__dirname+'/'+process.argv[2]).pipe(myVarStream) 
+		  .on('end', function () {
+			console.log(scope);
+			fs.writeFile(__dirname+"/"+process.argv[2].split('.')[0]+".json", JSON.stringify(scope), function(err) {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log("Saved!");
+				}
+			});
+			});
+		}
 	}
 else
 	console.log('No file input. Usage: node make_json.js file');
